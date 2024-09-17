5361ad550d7d305e1a131c24f0094dbe104c2fbc
JohannesHenz <if21b136@technikum-wien.at>
2024-09-17 21:28:47 +0200

    Initial Commit

---
+++ String_Class/String.cpp
@@ -0,0 +1,5 @@
+//
+// Created by johen on 17/09/2024.
+//
+
+#include "String.h"
---
+++ String_Class/String.h
@@ -0,0 +1,21 @@
+//
+// Created by johen on 17/09/2024.
+//
+
+#ifndef STRING_H
+#define STRING_H
+
+
+
+class String {
+
+    String(){};
+    ~String(){};
+
+
+
+};
+
+
+
+#endif //STRING_H
